d8778ddcadb994ff118487db3c40234bb73cd16b
Matyash Andrei <ratati12>
2021-02-21 05:36:56 -0800

    Moving dot for snake

--- 2.c
+++ 2.c
@@ -37,12 +37,25 @@ void up()
     while (1)
     {
         clear();
+        for (int i=0; i<COLS; i++)
+        {
+            mvaddch(0,i,'#');
+            mvaddch(LINES-1,i,'#');
+            mvaddch(i,0,'#');
+            mvaddch(i,COLS-1,'#');
+        }    
         y--;
         mvaddch(y,x,'x');
         refresh();
         usleep(100000);
         int ch;
         ch = getch();
+        
+
+        if (y <= 0) {y=LINES-2;}
+        else {if (y>=LINES -1) y=1;};
+        if (x <= 0) {x=COLS-1;}
+        else {if (x >= COLS-1) x=0;};
 
         if (ch == KEY_DOWN) down();
         if (ch == KEY_RIGHT) right();
@@ -56,13 +69,26 @@ void down()
 {   
     while (1)
     {
+
+
         clear();
+        for (int i=0; i<COLS; i++)
+        {
+            mvaddch(0,i,'#');
+            mvaddch(LINES-1,i,'#');
+            mvaddch(i,0,'#');
+            mvaddch(i,COLS-1,'#');
+        }    
         y++;
         mvaddch(y,x,'x');
         refresh();
         usleep(100000);
         int ch;
         ch = getch();
+        if (y <= 0) {y=LINES-2;}
+        else {if (y>=LINES -1) y=1;};
+        if (x <= 0) {x=COLS-1;}
+        else {if (x >= COLS-1) x=0;};
         if (ch == KEY_UP) up();
         if (ch == KEY_RIGHT) right();
         if (ch == KEY_LEFT) left();
@@ -74,12 +100,23 @@ void left()
     while (1)
     {
         clear();
+        for (int i=0; i<COLS; i++)
+        {
+            mvaddch(0,i,'#');
+            mvaddch(LINES-1,i,'#');
+            mvaddch(i,0,'#');
+            mvaddch(i,COLS-1,'#');
+        }    
         x--;
         mvaddch(y,x,'x');
         refresh();
         usleep(100000);
         int ch;
         ch = getch();
+        if (y <= 0) {y=LINES-2;}
+        else {if (y>=LINES -1) y=1;};
+        if (x <= 0) {x=COLS-1;}
+        else {if (x >= COLS-1) x=0;};
         if (ch == KEY_DOWN) down();
         if (ch == KEY_RIGHT) right();
         if (ch == KEY_UP) up();
@@ -91,12 +128,23 @@ void right()
     while (1)
     {
         clear();
+        for (int i=0; i<COLS; i++)
+        {
+            mvaddch(0,i,'#');
+            mvaddch(LINES-1,i,'#');
+            mvaddch(i,0,'#');
+            mvaddch(i,COLS-1,'#');
+        }    
         x++;
         mvaddch(y,x,'x');
         refresh();
         usleep(100000);
         int ch;
         ch = getch();
+        if (y <= 0) {y=LINES-2;}
+        else {if (y>=LINES -1) y=1;};
+        if (x <= 0) {x=COLS-1;}
+        else {if (x >= COLS-1) x=0;};
         if (ch == KEY_DOWN) down();
         if (ch == KEY_UP) up();
         if (ch == KEY_LEFT) left();
