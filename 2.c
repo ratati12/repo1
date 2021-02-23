2befb0cc35151d14d09dc61f28932a002ad0139f
Matyash Andrei <ratati12>
2021-02-22 16:07:04 -0800

    Final snake without apples + Coord and delay debugger

--- 2.c
+++ 2.c
@@ -4,23 +4,14 @@
 #include <unistd.h>
 #define LENG 7
 #define POINT '#'
-#define DELAY 700000
+#define DELAY 300000
 
 void up();
 void down();
 void left();
 void right();
 void paint_arena();
-void debug(int c)
-{
-int i;
-for (i=c; i>0; i--)
-{
-    mvprintw(2,COLS/2, "DEBUG: %d",i);
-    refresh();
-    sleep(1);
-}
-}
+void debug();
 
 struct coord 
 {
@@ -73,7 +64,12 @@ void up(struct coord snake)
             mvaddch(snake.y[k],snake.x[k],POINT);
         }
         usleep(DELAY);
+        mvprintw(1, 2,"x[MAX]=%d,y[MAX]=%d", COLS, LINES);
         refresh();
+        ch = getch();
+        if (ch == KEY_RIGHT) right(snake);
+        if (ch == KEY_LEFT) left(snake);
+        if (ch == 'q') {clear(); exit(0);} 
     }
         
     while (1)
@@ -87,6 +83,7 @@ void up(struct coord snake)
                 snake.x[i]=snake.x[i-1];
                 snake.y[i]=snake.y[i-1]+1;
         }
+
         mvprintw(1, 2,"x[MAX]=%d,y[MAX]=%d", COLS, LINES);
         
 
@@ -100,6 +97,7 @@ void up(struct coord snake)
         snake.y[0]--;
         refresh();
         usleep(DELAY);
+
 
         ch = getch();
         if (ch == KEY_RIGHT) right(snake);
@@ -137,6 +135,11 @@ void down(struct coord snake)
         }
         usleep(DELAY);
         refresh();
+        mvprintw(1, 2,"x[MAX]=%d,y[MAX]=%d", COLS, LINES);
+        ch = getch();
+        if (ch == KEY_RIGHT) right(snake);
+        if (ch == KEY_LEFT) left(snake);
+        if (ch == 'q') {clear(); exit(0);} 
     } 
     while (1)
     {
@@ -158,8 +161,8 @@ void down(struct coord snake)
         snake.y[0]++;
         refresh();
         usleep(DELAY);
+
         ch = getch();
-        
         if (ch == KEY_RIGHT) right(snake);
         if (ch == KEY_LEFT) left(snake);
         if (ch == 'q') {clear(); exit(0);} 
@@ -168,6 +171,34 @@ void down(struct coord snake)
 void left(struct coord snake)
 {   
     int i, ch;
+    int j;
+    int k;
+    for (i = LENG; i>0; i--)
+    {
+        clear();
+        paint_arena();
+        j=LENG-1;
+        while (j!= 0)
+        {
+            snake.x[j]=snake.x[j-1];
+            snake.y[j]=snake.y[j-1];
+            j--;
+        }
+        snake.x[0]--; 
+
+        for (k = 0; k <LENG; k++) 
+        {
+            mvprintw(k+2, 2,"x[%d]=%d,y[%d]=%d", k, snake.x[k],k, snake.y[k]);
+            mvaddch(snake.y[k],snake.x[k],POINT);
+        }
+        usleep(DELAY);
+        refresh();
+        mvprintw(1, 2,"x[MAX]=%d,y[MAX]=%d", COLS, LINES);
+        ch = getch();
+        if (ch == KEY_DOWN) down(snake);
+        if (ch == KEY_UP) up(snake);
+        if (ch == 'q') {clear(); exit(0);} 
+    }
     while (1)
     {
         clear();
@@ -189,6 +220,7 @@ void left(struct coord snake)
         snake.x[0]--;
         refresh();
         usleep(DELAY);
+
         ch = getch();
         if (ch == KEY_DOWN) down(snake);
         if (ch == KEY_UP) up(snake);
@@ -200,6 +232,34 @@ void left(struct coord snake)
 void right(struct coord snake)
 {   
     int i, ch;
+    int j;
+    int k;
+    for (i = LENG; i>0; i--)
+    {
+        clear();
+        paint_arena();
+        j=LENG-1;
+        while (j!= 0)
+        {
+            snake.x[j]=snake.x[j-1];
+            snake.y[j]=snake.y[j-1];
+            j--;
+        }
+        snake.x[0]++; 
+
+        for (k = 0; k <LENG; k++) 
+        {
+            mvprintw(k+2, 2,"x[%d]=%d,y[%d]=%d", k, snake.x[k],k, snake.y[k]);
+            mvaddch(snake.y[k],snake.x[k],POINT);
+        }
+        usleep(DELAY);
+        refresh();
+        mvprintw(1, 2,"x[MAX]=%d,y[MAX]=%d", COLS, LINES);
+        ch = getch();
+        if (ch == KEY_DOWN) down(snake);
+        if (ch == KEY_UP) up(snake);
+        if (ch == 'q') {clear(); exit(0);} 
+    }
     while (1)
     {
         clear();
@@ -226,6 +286,7 @@ void right(struct coord snake)
         if (ch == KEY_UP) up(snake);
         if (ch == 'q') {clear(); exit(0);} 
 
+
     }
 }
 
@@ -238,4 +299,16 @@ void paint_arena()
             mvaddch(i,0,'#');
             mvaddch(i,COLS-1,'#');
         }    
+}
+
+
+void debug(int c)
+{
+    int i;
+    for (i=c; i>0; i--)
+    {
+        mvprintw(2,COLS/2, "DEBUG: %d",i);
+        refresh();
+        sleep(1);
+    }
 }
